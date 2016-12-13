eae6056216e9595b98f3e92f0061b58022bee1d8
curiouskaran <01karan95@gmail.com>
2016-12-13 19:15:02 +0530

    udacity mini project 1

---
+++ message.py
@@ -0,0 +1,20 @@
+#mini project 1:- message for a friend
+
+import os
+
+def rename_files():
+
+#list all the files in the prank folder
+	file_list = os.listdir("/home/karan/pydev/decode/")
+	#print(file_list)
+	saved_path = os.getcwd()
+	print("current working directory is"+saved_path)
+	os.chdir("/home/karan/pydev/decode/")
+#remove all the numbers in the begining of the file name
+	for file_name in file_list:
+		print("old name->"+file_name)
+		print("new name->"+file_name.translate(None,"*#&0123456789"))
+		os.rename(file_name, file_name.translate( None, "*#&0123456789"))
+
+
+rename_files()
