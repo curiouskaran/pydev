c841244319ebc35bc1724c2bdbb658cfaf10202f
curiouskaran <01karan95@gmail.com>
2016-12-16 21:38:39 +0530

    better code for previous problem

--- python_practice/program_1.py
+++ python_practice/program_1.py
@@ -1,5 +1,6 @@
-#python practice - progrmam 1
+#python practice - program_1.py
 
+from datetime import date
 
 """Create a program that asks the user to enter their name and their age.
 Print out a message addressed to them that tells them the year that they will turn 100 years old."""
@@ -9,6 +10,6 @@ name = raw_input('--> ')
 print '\nHow old are you, %s?' % name,
 age = int(raw_input('-->'))
 
-year = 2016 + (100 -age)
+year = date.today().year + (100 -age)
 
-print '\ngreat %s you become 100 years old at %d'%(name, year)
+print '\ngreat %s you become 100 years old in %d'%(name, year)
