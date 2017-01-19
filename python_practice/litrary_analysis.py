c4ea20838a6f3af67777eca3b1c6f8f049a8941e
curiouskaran <01karan95@gmail.com>
2017-01-19 11:38:26 +0530

    literary analysis files

---
+++ python_practice/litrary_analysis.py
@@ -0,0 +1,5 @@
+#python_practice - litrary_analysis.py
+__author__ = 'karan sharma'
+
+'''You plan to write a program that will take a text file as input and return a report listing alphabetically all the words in the file and the number of occurances of each.'''
+
